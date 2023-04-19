928481f913cf842fd2a9063e5546e233fe32b876
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-19 20:39:00 +0100

    Add click to change colour and change colour on hover functionality

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function createGrid(size) {
+    // get square side length
     let axis = Math.sqrt(size)
     let container = document.getElementById("gridContainer");
+    //create x amount of pixels depending on size variable
     for (i=0;i<size;i++) {
         let pixel = document.createElement("div");
         pixel.setAttribute("class","gridPixel");
         pixel.setAttribute("id",`pixel-${i}`);
-        pixel.style.backgroundColor="red";
         container.appendChild(pixel);
-
 
     }
     container.style.display = "grid"
@@ -15,4 +15,28 @@ function createGrid(size) {
 
 }
 
+
+function listeners() {
+    //default colour
+    let pixelColour = "black"
+    //selects all pixels with gridPixel class
+
+
+    function changePaintColour() {
+        paintColour = alert("Enter hex code of colour")
+
+    }
+    function changePixelColour() {
+        this.style.backgroundColor = pixelColour;
+
+    }
+    //selects all elements with class gridPixel
+    pixels = document.querySelectorAll(".gridPixel")
+    pixels.forEach(pixel => { 
+        pixel.addEventListener('click', changePixelColour);
+        
+    });
+
+}
 createGrid(256)
+listeners()
