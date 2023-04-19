a94b98ffc3dcb7762cde6719c43905fd21a1cd48
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-19 19:27:23 +0100

    Give each pixel unique id

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function createGrid(size) {
     let axis = Math.sqrt(size)
     let container = document.getElementById("gridContainer");
-    for (i=1;i<size;i++) {
+    for (i=0;i<size;i++) {
         let pixel = document.createElement("div");
         pixel.setAttribute("class","gridPixel");
+        pixel.setAttribute("id",`pixel-${i}`);
+        pixel.style.backgroundColor="red";
         container.appendChild(pixel);
 
 
     }
     container.style.display = "grid"
-    container.style.gridTemplateColumns=`repeat(${axis}, 1fr)`
+    container.style.gridTemplateColumns=`repeat(${axis}, 10px)`
 
 }
 
